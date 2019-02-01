fc9e9099602cd372124ccfba75482ddc57103643
stoneage2003cn <13726251800>
2019-02-01 14:17:34 +0800

    1、优化 图片尺寸获取

--- apps/media_files/lib/handle_image.py
+++ apps/media_files/lib/handle_image.py
@@ -12,9 +12,44 @@ class HandleImage():
     def __init__(self,url=None):
         if url is None:
             url = BASE_IMAGE_URL
+        self.url = url
+        response = requests.get(self.url)
+        self.__image = Image.open(BytesIO(response.content))
+        self.size = self.__image.size
 
-    def thumbnails(self,url,handle):
-        response = requests.get(url)
-        image = Image.open(BytesIO(response.content))
-        image2 = image.resize((IMAGE_SIZE[handle]))
-        return image2
+
+
+    def himage(self,parameter):
+        """
+        :param url: 获取fastdfs的url地址
+        :param parameter: 指定操作参数
+        :return:返回图片
+        """
+        #获取执行参数
+        handle = IMAGE_SIZE[parameter][0]
+        if handle == "crop":
+            print(self.size)
+            box = self.clipimage()
+
+            pass
+        elif handle == "resize":
+            image2 = self.__image.resize((IMAGE_SIZE[parameter][1]), Image.ANTIALIAS)
+            image2.show()
+            print (self.size)
+            return image2
+            pass
+        else:
+            pass
+
+
+    def clipimage(self):
+        width = int(self.size[0])
+        height = int(self.size[1])
+        box = ()
+        if (width > height):
+            dx = width - height
+            box = (dx / 2, 0, height + dx / 2, height)
+        else:
+            dx = height - width
+            box = (0, dx / 2, width, width + dx / 2)
+        return box
